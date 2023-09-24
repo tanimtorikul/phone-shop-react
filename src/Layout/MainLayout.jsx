e351b27a913502325af39abe04612a9163c48849
torikulswe <torikulswe@gmail.com>
2023-09-24 22:06:40 +0600

    login route setup

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -4,7 +4,7 @@ import Header from "../components/Header/Header";
 
 const MainLayout = () => {
     return (
-        <div className="max-w-7xl mx-auto">
+        <div className="max-w-[1300px] mx-auto">
             <Header></Header>
             <Outlet></Outlet>
         </div>
